b6b4186af09d38c51e45798b07d61dd91dab4425
Rajesh Thomas <iamrajthomas@gmail.com>
2021-07-28 15:57:55 +0530

    Extract Console Output To File - First Commit

---
+++ WriteConsoleOutputToFile/ExtractConsoleOutputToFile.cs
@@ -0,0 +1,120 @@
+﻿//  -------------------------------------------------------------------------
+//  <copyright file="ExtractConsoleOutputToFile.cs"  author="Rajesh Thomas | iamrajthomas" >
+//      Copyright (c) 2021 All Rights Reserved.
+//  </copyright>
+// 
+//  <summary>
+//       ExtractConsoleOutputToFile is a dedicated class for Extracting Console Output To File
+//  </summary>
+//  -------------------------------------------------------------------------
+
+namespace WriteConsoleOutputToFile
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    /// <summary>
+    /// ExtractConsoleOutputToFile Class
+    /// </summary>
+    public class ExtractConsoleOutputToFile : TextWriter
+    {
+        #region Fields  
+        private Encoding encoding = Encoding.UTF8;
+        private StreamWriter streamWriter;
+        private TextWriter console;
+        #endregion
+
+        #region Properties  
+        /// <summary>
+        /// Encoding Property
+        /// </summary>
+        public override Encoding Encoding
+        {
+            get
+            {
+                return encoding;
+            }
+        }
+        #endregion
+
+        #region Constructors  
+        /// <summary>
+        /// ExtractConsoleOutputToFile Constructor
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="console"></param>
+        /// <param name="encoding"></param>
+        public ExtractConsoleOutputToFile(string filePath = "Output.txt", TextWriter console = null, Encoding encoding = null)
+        {
+            if (encoding != null)
+            {
+                this.encoding = encoding;
+            }
+            this.console = console ?? Console.Out;
+            this.streamWriter = new StreamWriter(filePath, false, this.encoding);
+            this.streamWriter.AutoFlush = true;
+        }
+        #endregion
+
+        #region Overrides  
+        /// <summary>
+        /// Write Method
+        /// </summary>
+        /// <param name="value"></param>
+        public override void Write(string value)
+        {
+            Console.SetOut(console);
+            Console.Write(value);
+            Console.SetOut(this);
+
+            if (this.streamWriter.BaseStream != null)
+                this.streamWriter.Write(value);
+        }
+
+        /// <summary>
+        /// WriteLine Method
+        /// </summary>
+        /// <param name="value"></param>
+        public override void WriteLine(string value)
+        {
+            Console.SetOut(console);
+            Console.WriteLine(value);
+
+            if (this.streamWriter.BaseStream != null)
+                this.streamWriter.WriteLine(value);
+            Console.SetOut(this);
+        }
+
+        /// <summary>
+        /// Flush
+        /// </summary>
+        public override void Flush()
+        {
+            this.streamWriter.Flush();
+        }
+
+        /// <summary>
+        /// Close
+        /// </summary>
+        public override void Close()
+        {
+            this.streamWriter.Close();
+        }
+        #endregion
+
+        #region IDisposable  
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        new public void Dispose()
+        {
+            this.streamWriter.Flush();
+            this.streamWriter.Close();
+            this.streamWriter.Dispose();
+            base.Dispose();
+        }
+
+        #endregion
+    }
+}
---
+++ WriteConsoleOutputToFile/Program.cs
@@ -0,0 +1,52 @@
+﻿//  -------------------------------------------------------------------------
+//  <copyright file="Program.cs"  author="Rajesh Thomas | iamrajthomas" >
+//      Copyright (c) 2021 All Rights Reserved.
+//  </copyright>
+// 
+//  <summary>
+//       Main Entry Point For The Application
+//  </summary>
+//  -------------------------------------------------------------------------
+
+namespace WriteConsoleOutputToFile
+{
+    using System;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This will be printed on Console but won't be Written on File - Before WriteTheConsoleDateToExternalFile() Invoke");
+            WriteTheConsoleDateToExternalFile();
+            Console.WriteLine("This will be printed on Console but won't be Written on File - After WriteTheConsoleDateToExternalFile() Invoke");
+        }
+
+        public static void WriteTheConsoleDateToExternalFile()
+        {
+            //This is sample for passing the params instead of default values
+            ExtractConsoleOutputToFile extractConsoleOutputToFile = new ExtractConsoleOutputToFile("Result.txt");
+            Console.SetOut(extractConsoleOutputToFile);
+
+            Console.WriteLine("Hello World - 1");
+            Console.WriteLine("Hello World - 2");
+            Console.WriteLine("Hello World - 3");
+            Console.WriteLine("Hello World - 4");
+            Console.WriteLine("Hello World - 5");
+            Console.WriteLine("Hello World - 6");
+            Console.WriteLine("Hello World - 7");
+            Console.WriteLine("Hello World - 8");
+            Console.WriteLine("Hello World - 9");
+            Console.WriteLine("Hello World - 10");
+            Console.WriteLine("Hello World - 11");
+            Console.WriteLine("..");
+            Console.WriteLine("..");
+            Console.WriteLine("..");
+            Console.WriteLine("..");
+            Console.WriteLine("..");
+            Console.WriteLine("..");
+            Console.WriteLine("Hello World - 100");
+            Console.WriteLine("Hello World - 101");
+
+            extractConsoleOutputToFile.Close();
+        }
+    }
+}
